3fa95d5279aaa42bd3c159c2252f310747e03bc5
Elwin Dela Cruz <elwindelacruz@Elwins-MacBook-Pro.local>
2022-12-01 01:25:28 +0800

    develop mobile version of all the section includes hamburger menu

--- js/custom.js
+++ js/custom.js
@@ -171,6 +171,33 @@ var threaded = {
 		})
 	},
 
+	clickable: function() {
+
+		$('.js-hamburger-main').click(function(e){
+			e.preventDefault();
+
+			$(this).toggleClass('active');
+			$('.main-menu').toggleClass('active');
+		});
+
+
+		$('.custom-currency li a').click(function(){
+			var sitem = $(this).html(),
+				$this = $(this);
+	
+			$('.switch-language .js-mobile-custom-dropdown ul li').removeClass('selected');
+			$(this).parent().addClass('selected');
+			$(this).closest('ul').siblings('span.selected').html(sitem);
+			$(this).closest('ul').slideUp(150);
+			$(this).children('ul').stop(true, false).slideToggle(150);
+	
+		});
+	
+		$('.custom-currency').click(function() {
+			$(this).children('ul').stop(true, false).slideToggle(150);
+		});
+	},
+
 	coverImage: function(image) {
 		var _banner = {
 			init: function() {
@@ -237,6 +264,7 @@ var threaded = {
 		threaded.entranceAnimation();
 		threaded.coverImage();
 		threaded.bannerCarousel();
+		threaded.clickable();
 	}
 };
 
@@ -244,6 +272,15 @@ if(window.jQuery) {
     threaded.init();
 }
 
-$(document).ready(function(){
-	
-  });
+$(document).on({
+	mouseenter: function(){
+		clearTimeout(timer);
+		$('.custom-dropdown-menu', this).slideDown(150);
+
+	},
+	mouseleave: function(){
+		timer = setTimeout(function(){
+			$('.custom-dropdown-menu').slideUp(150);
+		}, 1500);
+	}
+}, '.custom-dropdown-menu');
